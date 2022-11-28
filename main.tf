03933a33d0686c27b9ed4caae892ce57d2805025
bywhite <bywhite@cisco.com>
2022-11-27 21:31:15 -0600

    separate file for pool configs

--- main.tf
+++ main.tf
@@ -54,12 +54,12 @@ module "intersight_policy_bundle" {
 
 
 # starting values for wwnn, wwpn-a/b and mac pools (size 255)
-  wwnn-block   = "20:00:00:CA:FE:00:00:01"
-  wwpn-a-block = "20:00:00:CA:FE:0A:00:01"
-  wwpn-b-block = "20:00:00:CA:FE:0B:00:01"
+  # wwnn-block   = "20:00:00:CA:FE:00:00:01"
+  # wwpn-a-block = "20:00:00:CA:FE:0A:00:01"
+  # wwpn-b-block = "20:00:00:CA:FE:0B:00:01"
 
 # vNIC Starting MAC Block
-  mac-block    = "00:CA:FE:00:00:01"
+  # mac-block    = "00:CA:FE:00:00:01"
 
 
 # IMC and FI environment specifics
